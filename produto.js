e67bc9a30a8cf4369a6ea03f2e1b9f56068fbf57
Italo Ferreira <itoferreira0620@gmail.com>
2025-06-09 16:00:08 -0300

    commit 3: criação da classe Produto com validação

--- produto.js
+++ produto.js
@@ -1,1 +1,14 @@
- 
+ class Produto {
+    constructor(id, nome, preco, categoria) {
+        if (preco <= 0) {
+            throw new Error("Preço inválido. Deve ser maior que zero.");
+        }
+
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+ }
+
+ module.exports = Produto;
