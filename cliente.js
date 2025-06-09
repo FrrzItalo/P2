8ec726dbea1f92e118b01e4b0da48a743a562f14
Italo Ferreira <itoferreira0620@gmail.com>
2025-06-09 15:52:58 -0300

    commit 2:criação da classe cliente

--- cliente.js
+++ cliente.js
@@ -1,1 +1,10 @@
- 
+ class Cliente {
+    constructor(id, nome, email, cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+    }
+ }
+
+ module.exports = Cliente;
