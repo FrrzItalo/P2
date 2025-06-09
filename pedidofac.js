11414d6220a2decf2306a5ee29e6865bc9cc369d
Italo Ferreira <itoferreira0620@gmail.com>
2025-06-09 16:44:17 -0300

    commit 6: Criação pedidofac

--- pedidofac.js
+++ pedidofac.js
@@ -1,1 +1,9 @@
- 
+const pedido = require('./pedido');
+
+class PedidoFactory {
+    static criarPedido(id, cliente, produtos, data) {
+        return new pedido(id, cliente, produtos, data);
+    }
+}
+
+module.exports = PedidoFactory;
