85db88bd48a0bb4543ae6b59ce768a772a67cddf
Italo Ferreira <itoferreira0620@gmail.com>
2025-06-09 16:07:39 -0300

    commit 4: criação da classe pedido com cálculo de total

--- pedido.js
+++ pedido.js
@@ -1,1 +1,15 @@
- 
+class pedido {
+    constructor(id, nome, produto, data) {
+        this.id = id;
+        this.cliente = this.cliente;
+        this.produto = produto;
+        this.data = data;
+        this.valorTotal = this.calculaTotal();
+    }
+
+    calculaTotal() {
+        return this.produtos.reduce((total, produto) => total + produto.preco, 0);
+    }
+} 
+
+module.exports = pedido;
